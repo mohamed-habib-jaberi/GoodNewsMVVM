acf305e4c4ca776b7a0ee22e60049da06ef5c9a0
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-07-20 22:12:56 +0200

    Displaying News

--- GoodNewsMVVM/Controllers/NewsListTableViewController.swift
+++ GoodNewsMVVM/Controllers/NewsListTableViewController.swift
@@ -11,19 +11,47 @@ import UIKit
 
 class NewsListTableViewController: UITableViewController {
     
+    private var articleListVM : ArticleListViewModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
     }
     
-     private func setup(){
+    private func setup(){
         self.navigationController?.navigationBar.prefersLargeTitles = true
-
-       //change the date from=2020-04-04 By current date
-               let url = URL(string: "http://newsapi.org/v2/everything?q=bitcoin&from=2020-20-07&sortBy=publishedAt&apiKey=eb10f138ce4c4fa7b4187ddefc69bb67")!
-        WebService().getArticles(url: url) { _ in
+        
+        //change the date from=2020-04-04 By current date
+        let url = URL(string: "http://newsapi.org/v2/everything?q=bitcoin&from=2020-20-07&sortBy=publishedAt&apiKey=eb10f138ce4c4fa7b4187ddefc69bb67")!
+        
+        WebService().getArticles(url) { (articles) -> (Void) in
             
+            if let articles = articles {
+                self.articleListVM = ArticleListViewModel(articles: articles)
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+                
+            }
         }
+        
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.articleListVM == nil ? 0 : self.articleListVM.numberOfSections
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.articleListVM.numberOfRowsInSection(section)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = self.tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as? ArticleTableViewCell else { fatalError("ArticleTableViewCell is not found")  }
+        
+        let articleVM = self.articleListVM.articleAtIndex(indexPath.row)
+        cell.titleLabel.text = articleVM.title
+        cell.descriptionLabel.text = articleVM.description
+        return cell
     }
 }
--- GoodNewsMVVM/Services/WebService.swift
+++ GoodNewsMVVM/Services/WebService.swift
@@ -9,7 +9,7 @@
 import Foundation
 class WebService {
     
-    func getArticles(url: URL, completion: @escaping ([Article]?) -> (Void))  {
+    func getArticles(_ url: URL, completion: @escaping ([Article]?) -> (Void))  {
         
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             
