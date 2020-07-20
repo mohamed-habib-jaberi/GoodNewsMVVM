0d3c9b27bf905977cba53e1ef3e0f8d41f5583aa
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-07-20 21:55:18 +0200

    Creating Custom Self Sizing Cell

---
+++ GoodNewsMVVM/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleTableViewCell.swift
+//  GoodNewsMVVM
+//
+//  Created by mohamed  habib on 20/07/2020.
+//  Copyright © 2020 mohamed  habib. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
